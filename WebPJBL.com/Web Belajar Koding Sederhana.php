a8000e5b62da35da4a9b9f022c57d4dcf3508152
XD12ok <arsasupergg@gmail.com>
2024-10-08 11:51:06 +0700

    Add files via upload

---
+++ WebPJBL.com/Web Belajar Koding Sederhana.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Belajar Koding Dasar</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+<img src="Untitled design (19).png" width="61" height="59" alt=""/>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item"> </li>
+            <li class="nav-item dropdown">
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item"> </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    <div class="container mt-3">
+      <div class="row">
+        <div class="col-12">
+          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleControls" data-slide-to="1"></li>
+              <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="images/1920x500.gif" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Item 1 Heading</h5>
+                  <p>Item 1 Description</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/1920x500.gif" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Item 2 Heading</h5>
+                  <p>Item 2 Description</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="images/1920x500.gif" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h5>Item 3 Heading</h5>
+                  <p>Item 3 Description</p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <hr>
+    </div>
+<hr>
+    <h2 class="text-center">MATERI PEMBELAJARAN&nbsp;</h2>
+    <hr>
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Judul Materi</h5>
+              <p class="card-text">deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Judul Materi</h5>
+              <p class="card-text">deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">judul materi</h5>
+              <p class="card-text">deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center mt-4">
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">judul materi</h5>
+              <p class="card-text">deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Judul Materi</h5>
+              <p class="card-text">Deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 pb-1 pb-md-0">
+          <div class="card">
+            <img class="card-img-top" src="images/400X200.gif" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Judul materi</h5>
+              <p class="card-text">Deskripsi materi</p>
+              <a href="#" class="btn btn-primary">Buka&nbsp;</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <h2 class="text-center">NEW UPDATE COMING SOON&nbsp;&nbsp;</h2>
+    <hr>
+<hr>
+<footer class="text-center">
+  <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <p>Made By Arsa Pradityo Nugroho&nbsp;</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap-4.4.1.js"></script>
+  </body>
+</html>
